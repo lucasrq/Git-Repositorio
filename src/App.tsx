7a3da29fd9664e2b6c35bfd55423c3edb12f87a5
lucasrq <lucasroque545@gmail.com>
2024-04-23 17:43:02 -0300

    new

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Teste from './teste';
+import EstiloGlobal from './styles'
+function App() {
+  return (
+
+    <>
+      <Teste />
+      <EstiloGlobal />
+    </>
+
+  );
+}
+
+export default App;
---
+++ src/styles.ts
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from 'styled-components'
+const EstiloGlobal = createGlobalStyle`
+*{
+  padding:0;
+  margin:0;
+}
+`
+export default EstiloGlobal
